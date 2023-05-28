259fe92e97f49342306bdb122cdcbb1280265c8c
Agustin faucetta <agusfau@gmail.com>
2023-05-27 23:57:36 -0300

    nuevo

---
+++ index.js
@@ -0,0 +1,11 @@
+function add3Points () {
+    myPoints = myPoints + 3
+}
+function remove1Point(){
+    myPoints = myPoints - 1
+}
+add3Points();
+add3Points();
+add3Points();
+remove1Point();
+remove1Point();

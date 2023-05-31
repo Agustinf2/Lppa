068c07935849fe4dbce04bf8726d964b8db02cde
Agustin faucetta <agusfau@gmail.com>
2023-05-31 18:49:52 -0300

    prueba

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ add3Points();
 add3Points();
 remove1Point();
 remove1Point();
+function add(){
+    console.log("button clicked")
+    let suma = num1 + num2
+    sumEl.textContent = suma
+    //sumEl.innerText = num1 + num2
+}
+
+function subtract(){
+    console.log("button clicked") 
+    let resta = num1 - num2  
+    sumEl.textContent = resta
+    //sumEl.innerText = num1 - num2
+}

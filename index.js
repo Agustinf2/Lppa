ea143886a719097158e6f62152465cd96f37088b
Agustin faucetta <agusfau@gmail.com>
2023-06-14 23:05:01 -0300

    nuevp

--- index.js
+++ index.js
@@ -26,3 +26,12 @@ function subtract(){
 //<option value="">Opcion 3</option>
 //<option value="">Opcion 4</option>
 //<option value="">Opcion 5</option>
+let castle = {
+    title: "Live like a king in my castle",
+    price: 190,
+    isSuperHost: true,
+    images: ["img/castle1.png", "img/caste2.png"]
+}
+
+console.log(castle.price)
+console.log(castle.isSuperHost)

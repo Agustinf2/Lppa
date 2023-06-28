b44a8066f624740391d2390a76101105432c70c0
Agustin faucetta <agusfau@gmail.com>
2023-06-28 20:20:07 -0300

    estilo

---
+++ Practica/index.js
@@ -0,0 +1,49 @@
+function add3Points () {
+    myPoints = myPoints + 3
+}
+function remove1Point(){
+    myPoints = myPoints - 1
+}
+add3Points();
+add3Points();
+add3Points();
+remove1Point();
+remove1Point();
+function add(){
+    console.log("button clicked")
+    let suma = num1 + num2
+    sumEl.textContent = suma
+    //sumEl.innerText = num1 + num2
+}
+
+function subtract(){
+    console.log("button clicked") 
+    let resta = num1 - num2  
+    sumEl.textContent = resta
+    //sumEl.innerText = num1 - num2
+}
+
+//<option value="">Opcion 3</option>
+//<option value="">Opcion 4</option>
+//<option value="">Opcion 5</option>
+let castle = {
+    title: "Live like a king in my castle",
+    price: 190,
+    isSuperHost: true,
+    images: ["img/castle1.png", "img/caste2.png"]
+}
+
+console.log(castle.price)
+console.log(castle.isSuperHost)
+
+let experience = ["Mary Leiva",
+"4to de facultad",
+"i can program in c# or javascript"]
+
+console.log (experience [1])
+
+
+let mySelf = [
+    "Agustin Faucetta",
+    23,
+]
